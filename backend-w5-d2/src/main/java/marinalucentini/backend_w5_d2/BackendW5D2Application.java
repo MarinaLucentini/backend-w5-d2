058ce99e69e0d61e954133d67bc8b51cc9db62ec
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-06-18 14:05:08 +0200

    Create project

---
+++ backend-w5-d2/src/main/java/marinalucentini/backend_w5_d2/BackendW5D2Application.java
@@ -0,0 +1,13 @@
+package marinalucentini.backend_w5_d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendW5D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendW5D2Application.class, args);
+	}
+
+}
---
+++ backend-w5-d2/src/test/java/marinalucentini/backend_w5_d2/BackendW5D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package marinalucentini.backend_w5_d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendW5D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
